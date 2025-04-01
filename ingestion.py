ecc4b04274eff809c2f919400c12352d0289739e
Samir Satam <ssatam@gmail.com>
2025-03-31 21:41:48 -0400

    Trying to classify Federalist papers.

--- ingestion.py
+++ ingestion.py
@@ -1,7 +1,9 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
+from langchain_experimental.text_splitter import SemanticChunker
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
@@ -9,15 +11,17 @@ from langchain_pinecone import PineconeVectorStore
 load_dotenv()
 
 if __name__ == '__main__':
-    print("Hello World")
-    loader = TextLoader("./mediumblog1.txt")
-    document = loader.load() # loads in a LangChain document.
+    pathlist = Path("./federalist_papers").iterdir()
+    for path in pathlist:
+        if path.is_file():
+            loader = TextLoader(path)
+            document = loader.load() # loads in a LangChain document.
 
-    print("Splitting")
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_documents(document)
-    print(f"created {len(texts)} chunks")
+            # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            embeddings = OpenAIEmbeddings()
+            text_splitter = SemanticChunker(embeddings=embeddings)
+            texts = text_splitter.split_documents(document)
+            print(f"created {len(texts)} chunks")
 
-    embeddings = OpenAIEmbeddings() #(openai_api_key=os.environ.get("OPENAI_API_KEY"))
-    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ["INDEX_NAME"])
-    print("Finished")
+            PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ["FEDERALIST_PAPERS_INDEX_NAME"])
+            print("Finished")
--- main.py
+++ main.py
@@ -15,53 +15,63 @@ load_dotenv()
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
+
 def main():
     print("Hello from intro-to-vector-dbs!")
 
     embeddings = OpenAIEmbeddings()
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(temperature=0)
 
-    query = "what is Pinecone in machine learning?"
-    chain = PromptTemplate.from_template(template=query,) | llm
-    result = chain.invoke(input={})
-    print("--------------- Not Using RAG -------------------")
-    print(result.content)
+    # Madison wrote 49–58
+    # John Jay wrote 64
+    query = "Who wrote Federalist No. 64?"
+    # Madison and Hamilton wrote 18-20
+    query = "Who wrote Federalist No. 18?"
+    # query = "Give details about the paper titled 'Harry Potter'"
+    query = "What paper mentions the text - 'The internal effects of a mutable policy are still more calamitous'"
 
     vectorstore = PineconeVectorStore(
-        index_name=os.environ["INDEX_NAME"], embedding=embeddings
+        index_name=os.environ["FEDERALIST_PAPERS_INDEX_NAME"], embedding=embeddings
     )
-
-    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
-    retrieval_chain = create_retrieval_chain(
-        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
-    )
-    result = retrieval_chain.invoke(input={"input": query})
-    print("--------------- After Using RAG -------------------")
-    print(result['answer'])
 
     # custom prompt
     template = """Use the following pieces of context to answer the question at the end.
     If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    Use three sentences maximum, and keep the answer as concise as possible.
-    Always say "thanks for asking!" at the end of the answer.
-    
+    You are a linguistic expert, and a historian. You can analyze patterns in text and attribute them to a specific person or writer.
+    You also know that the writers of the Federalist papers are John Jay, Alexander Hamilton, and James Madison.
+    You are an expert historian as well, and know a lot about the writing styles of John Jay, Alexander Hamilton, and James Madison.
+    Sometimes a Federalist paper has a single author or it is collaboration between multiple authors.
+    You will follow the steps below to answer the question at the end.
+    1. Figure out if the Authorship is ambiguous or not.
+    3. If there is no ambiguity about the authorship, just output the author or authors as is.
+    2. Remember that there could be multiple authors for a single paper. If there was an assistance from another author, return both authors.
+    4. If there is ambiguity about the authorship, use your expertise about the style of writing and analyze the text to resolve the ambiguity and determine the correct author.
     {context}
+    5. Describe the steps you followed to answer the question.
     
     Question: {question}
     Helpful Answer:"""
 
     #make the text into a prompt
-    custom_rag_prompt  = PromptTemplate.from_template(template=template)
-    # make up the RAG chain, format_docs == all the documents appended together.
-    rag_chain = (
-        {"context": vectorstore.as_retriever() | format_docs, "question": RunnablePassthrough()}
-        | custom_rag_prompt
-        | llm
-    )
+    custom_rag_prompt = PromptTemplate.from_template(template=template)
 
-    res = rag_chain.invoke(query)
-    print(res)
+    combine_docs_chain = create_stuff_documents_chain(llm, custom_rag_prompt)
+    retrieval_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    )
+    res = retrieval_chain.invoke(input={"input": query, "question": query})
+
+    # make up the RAG chain, format_docs == all the documents appended together.
+    # rag_chain = (
+    #         {"context": vectorstore.as_retriever() | format_docs, "question": RunnablePassthrough()}
+    #         | custom_rag_prompt
+    #         | llm
+    # )
+    #
+    # res = rag_chain.invoke(query)
+    print(f'Context: {res["context"]}\n\n')
+    print(res['answer'])
+
 
 if __name__ == "__main__":
     main()
